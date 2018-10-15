ecbb371a4a03b18ef86f20b275840fa95b2612c8
yanis <yanis.hammada@ricardo.ch>
2018-10-15 23:15:55 +0200

    Basic skeleton

---
+++ main.c
@@ -0,0 +1,159 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * InputBuffer represents data input by the user
+  * buffer contains the input itself
+  * buffer_length is the total size of the buffer
+  * input_length is the size of the user input
+**/
+struct InputBuffer_t
+{
+  char *buffer;
+  size_t buffer_length;
+  ssize_t input_length;
+};
+typedef struct InputBuffer_t InputBuffer;
+
+/**
+ * MetaCommandResult represents the output of a
+ * meta command execution
+ * A metacommand starts with a doc: `.exit`
+**/
+enum MetaCommandResult_t
+{
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+};
+typedef enum MetaCommandResult_t MetaCommandResult;
+
+/**
+ * PrepareResult represents the output of a
+ * SQL query analysis
+**/
+enum PrepareResult_t
+{
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT
+};
+typedef enum PrepareResult_t PrepareResult;
+
+/**
+ * StatementType represents all the suported SQL statements
+ **/
+enum StatementType_t
+{
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+};
+typedef enum StatementType_t StatementType;
+
+struct Statement_t
+{
+  StatementType type;
+};
+typedef struct Statement_t Statement;
+
+InputBuffer *new_input_buffer()
+{
+  InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+  return input_buffer;
+}
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+  if (strcmp(input_buffer->buffer, ".exit") == 0)
+  {
+    exit(EXIT_SUCCESS);
+  }
+  else
+  {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+  {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+
+  if (strcmp(input_buffer->buffer, "select") == 0)
+  {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+  switch (statement->type)
+  {
+  case (STATEMENT_INSERT):
+    printf("This is where we would do an insert.\n");
+    break;
+  case (STATEMENT_SELECT):
+    printf("This is where we would do a select.\n");
+    break;
+  }
+}
+
+void print_prompt()
+{
+  printf("db > ");
+}
+
+void read_input(InputBuffer *input_buffer)
+{
+  size_t read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+  input_buffer->input_length = read - 1;
+  input_buffer->buffer[read - 1] = '\0';
+}
+
+int main(int argc, char *argv[])
+{
+  InputBuffer *input_buffer = new_input_buffer();
+  while (true)
+  {
+    print_prompt();
+    read_input(input_buffer);
+
+    // Meta commands
+    if (input_buffer->buffer[0] == '.')
+    {
+      switch (do_meta_command(input_buffer))
+      {
+      case META_COMMAND_SUCCESS:
+        continue;
+      case META_COMMAND_UNRECOGNIZED_COMMAND:
+        printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        continue;
+      }
+    }
+
+    // SQL query
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement))
+    {
+    case PREPARE_SUCCESS:
+      break;
+
+    case PREPARE_UNRECOGNIZED_STATEMENT:
+      printf("Unrecognized keyword at start of '%s'.\n",
+             input_buffer->buffer);
+      continue;
+    }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
+  }
+}
